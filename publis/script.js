7e5477140e810f121e6233f98de9ba766b6ff0a1
Prateek Bhatnagar <prateek89born@gmail.com>
2017-07-06 16:18:16 +0530

    experimenting collision

--- publis/script.js
+++ publis/script.js
@@ -10,9 +10,22 @@ function init() {
   circle.graphics
   .beginLinearGradientFill(["#4A148C","#3F51B5","#00BFA5"], [0, .2, 1], 0, 0, 0, 600)
   .drawRect(0, 0, 600, 600);
-  // stage.addEventListener('click',(e)=>{
-
-  // });
+  stage.addEventListener('click',(e)=>{
+		const newX = bitmap.x>150?100:300;
+		const newRotation = bitmap.x===100?45:-45;
+		const rocket = createjs.Tween.get(bitmap);
+		rocket
+			.to({
+				rotation: newRotation,
+			}, 0)
+			.to({
+				x: newX,
+				y: bitmap.y - 50
+			}, 400)
+			.to({
+				rotation: 0,
+			}, 100);
+  });
   stage.addChild(circle);
   stage.update();
   addLine();
@@ -35,6 +48,7 @@ function addLine(){
   addRocket();
   createjs.Ticker.addEventListener("tick", ()=>{
     lines.forEach(l=>{
+			console.log(ndgmr.checkRectCollision(bitmap,l));
       l.y += 3;
     });
     if (line[0] > 700) {
@@ -43,19 +57,16 @@ function addLine(){
     if (lines[lines.length -1] && lines[lines.length -1].y > -30) {
       pushIntoLines();
     }
-    bitmap.y += 0.5;
+    bitmap.y += 1;
     stage.update();
   });
 }
 
 function pushIntoLines(){
-  if (!bool) {
-    //return;
-  }
   var line = new createjs.Shape();
   line.graphics.setStrokeStyle(3);
   line.graphics.beginStroke(lineColors[Math.floor(Math.random()*3)]);
-  const height = Math.floor(30+Math.random()*100);
+  const height = Math.floor(20+Math.random()*50);
   line.graphics.moveTo(200, -1 * height);
   line.graphics.lineTo(200, 0);
   line.height = height;
@@ -63,12 +74,11 @@ function pushIntoLines(){
   line.graphics.endStroke();
   lines.push(line);
   stage.addChild(line);
-  bool = false;
 }
 
 function addRocket(){
   var doge = new Image();
-  doge.src = "http://i.imgur.com/0KuPCEj.png";
+  doge.src = "/rocket.png";
   bitmap = new createjs.Bitmap(doge);
   bitmap.scaleY = 0.075;
   bitmap.scaleX = 0.08;
