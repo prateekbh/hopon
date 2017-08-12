1120f7018ddf762be154a689dd2edf27706f7f3a
Prateek Bhatnagar <prateek89born@gmail.com>
2017-08-12 22:30:59 +0530

    WIP building stage

--- game/Ball.js
+++ game/Ball.js
@@ -1,3 +1,24 @@
+import {
+	Plane,
+	Engine,
+	Scene,
+	HemisphericLight,
+	Vector3,
+	Color3,
+	FreeCamera,
+	ArcRotateCamera,
+	Mesh,
+	MeshBuilder,
+	PhysicsImpostor,
+	CannonJSPlugin,
+	Texture,
+	StandardMaterial,
+	ShadowGenerator,
+	DirectionalLight,
+	Color4,
+	CSG,
+} from 'babylonjs';
+
 class Ball{
 	constructor(){
 
--- game/Stage.js
+++ game/Stage.js
@@ -1,7 +1,35 @@
-class Stage {
-	constructor(){
+import {
+	Engine,
+	Scene,
+	HemisphericLight,
+	Vector3,
+	FreeCamera,
+	ShadowGenerator,
+} from 'babylonjs';
 
+class Stage {
+
+	constructor(canvas){
+		const engine = new Engine(canvas);
+		this.scene = new Scene(engine);
+		this.ground = Mesh.CreateGround("ground1", 10, 30, 2, scene);
+		addLights(scene);
+		setCamera(scene);
 	}
+
+	addLights(){
+		const light = new HemisphericLight("light", new Vector3(0, 1, 0), this.scene);
+		light.intensity = 0.5;
+		const shadowGenerator = new ShadowGenerator(1024, light);
+	}
+
+	setCamera(){
+		const camera = new FreeCamera("FreeCamera",new Vector3(0, 2, 10), this.scene);
+		camera.setTarget(new Vector3(0, 5, -10));
+		camera.attachControl(canvas, false);
+		camera.setTarget(Vector3.Zero());
+	}
+
 }
 
 export default Stage;
